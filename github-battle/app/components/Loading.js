1492871dc1ccead327b89a42859a26019383cf09
Martin Rodin <rodin.martin@gmail.com>
2018-06-06 19:10:09 +0200

    Refactor to use Class Properties (Class Fields)

--- github-battle/app/components/Loading.js
+++ github-battle/app/components/Loading.js
@@ -9,12 +9,18 @@ var styles = {
 };
 
 class Loading extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: props.text
-    };
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  }
+  
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
+  }  
+  
+  state = {
+    text: this.props.text
   }
 
   componentDidMount() {
@@ -39,16 +45,6 @@ class Loading extends React.Component {
       </p>
     )
   }
-}
-
-Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
-}
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
 }
 
 export default Loading;
--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -44,15 +44,11 @@ Player.propTypes = {
 }
 
 class Results extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      winner: null,
-      loser: null,
-      error: null,
-      loading: true
-    }
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
   }
 
   componentDidMount() {
