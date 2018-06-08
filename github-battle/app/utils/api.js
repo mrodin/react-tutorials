173a04793fc7e1875c461296a185e40d7f200bd5
Martin Rodin <rodin.martin@gmail.com>
2018-06-08 22:46:50 +0200

    Refactor from axios to fetch

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-
 async function getProfile(username) {
-  const profile = await axios.get(`https://api.github.com/users/${username}`);
+  const response = await fetch(`https://api.github.com/users/${username}`);
 
-  return profile.data;
+  return response.json();
 }
 
-function getRepos(username) {
-  return axios.get(`https://api.github.com/users/${username}/repos?per_page=100`);
+async function getRepos(username) {
+  const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
+
+  return response.json();
 }
 
 function getStarCount(repos) {
-  return repos.data.reduce((count, repo) => count + repo.stargazers_count, 0);
+  return repos.reduce((count, repo) => count + repo.stargazers_count, 0);
 }
 
 function calculateScore(profile, repos) {
@@ -53,8 +53,9 @@ export async function battle(players) {
 
 export async function fetchPopularRepos(language) {
   const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
-  const repos = await axios.get(encodedURI)
+  const response = await fetch(encodedURI)
     .catch(handleError);
     
-  return repos.data.items;
+  const repos = await response.json();
+  return repos.items;
 }
